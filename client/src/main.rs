9f8cfe8d1412800144abd59021efd4c86e7c4788
Vladislav Sumin <vladislavsumin@mail.ru>
2023-08-23 22:18:53 +0300

    add game state

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,19 @@
 use bevy::prelude::*;
 
+/// Глобальное состояние игры
+#[derive(States, Clone, Eq, PartialEq, Hash, Default, Debug)]
+enum AppState {
+    /// Первичная загрузка игры (еще до показа основного игрового меню)
+    #[default]
+    InitialLoading,
+
+    /// Основное игровое меню
+    MainMenu,
+}
+
 fn main() {
     App::new()
+        .add_plugins(DefaultPlugins)
+        .add_state::<AppState>()
         .run();
 }
